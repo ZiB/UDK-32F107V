7c2a80fdb899f4963efe3ff83157967fa22a0e26
Denis Zheleznyakov <zheleznjakov@mail.ru>
2014-06-27 08:08:37 +0700

    + шаблон проекта

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+ * File: main.c
+ * Date: 09.11.2013
+ * Denis Zheleznyakov aka ZiB @ http://ziblog.ru
+ */
+
+#include "main.h"
+
+void main(void)
+{
+	while (1)
+	{
+	}
+}
---
+++ main.h
@@ -0,0 +1,12 @@
+/*
+ * File: main.h
+ * Date: 09.11.2013
+ * Denis Zheleznyakov aka ZiB @ http://ziblog.ru
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+
+#endif /* MAIN_H_ */
